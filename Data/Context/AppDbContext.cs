3d3e74aebfc34231d5a989cc672f0db58afa3d26
umitd111 <umit.demirci1996@gmail.com>
2023-05-31 00:27:19 +0300

    DbContext is created. First Migration is done.

---
+++ Data/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        //Creating Table Name. It is Articles here. It is plural bc there will be more than one article in db
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
+    }
+}

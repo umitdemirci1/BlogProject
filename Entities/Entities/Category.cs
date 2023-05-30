ce3bde70ee31ba81fc334db061764fd6428aaf48
umitd111 <umit.demirci1996@gmail.com>
2023-05-30 18:03:31 +0300

    Entities and common entity base are created.

---
+++ Entities/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+
+namespace Entity.Entities
+{
+    public class Category: EntityBase, IEntityBase
+    {
+        public string Name { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}

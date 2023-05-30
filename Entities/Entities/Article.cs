43f38d051b091f6a2407e5be3007ada9cbb6a028
umitd111 <umit.demirci1996@gmail.com>
2023-05-30 22:35:29 +0300

    Minor cleaning

--- Entities/Entities/Article.cs
+++ Entities/Entities/Article.cs
@@ -2,7 +2,7 @@
 
 namespace Entity.Entities
 {
-    public class Article : EntityBase, IEntityBase
+    public class Article : EntityBase
     {
         public string Title { get; set; }
         public string Content { get; set; }

987994490c10d71ad7e5d41152f723ddf439ad1a
umitd111 <umit.demirci1996@gmail.com>
2023-06-03 18:37:44 +0300

    Testing to the mapping

---
+++ Data/Mappings/ArticleMap.cs
@@ -0,0 +1,14 @@
+﻿using Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Mappings
+{
+    public class ArticleMap : IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            builder.Property(x => x.Title).HasMaxLength(150);
+        }
+    }
+}
